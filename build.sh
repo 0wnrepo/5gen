f309c7af80e40fbce128c5537c1b0121159e056b
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-05-02 19:19:19 -0400

    update to use latest repos

--- build.sh
+++ build.sh
@@ -16,12 +16,14 @@ export LDFLAGS=-L$builddir/lib
 
 echo builddir = $builddir
 
+echo building libaesrand
 cd libaesrand
     ./configure --prefix=$builddir
     make
     make install
 cd ..
 
+echo building clt13
 cd clt13
     mkdir -p build/autoconf
     autoreconf -i
@@ -30,14 +32,30 @@ cd clt13
     make install
 cd ..
 
+echo building gghlite-flint
 cd gghlite-flint
+    autoreconf -i
+    ./configure --prefix=$builddir
+    make -j
+    make install
+cd ..
+
+cd libmmap
+    autoreconf -i
+    ./configure --prefix=$builddir
+    make
+    make install
+cd ..
+
+cd mife
     git submodule init
     git submodule update
+    mkdir -p build/autoconf
     autoreconf -i
     ./configure --prefix=$builddir
     sed '/all:/i install:' mife/jsmn/Makefile > tmp-jsmn-makefile
     mv tmp-jsmn-makefile mife/jsmn/Makefile
-    make -j
+    make
     make install
 cd ..
 
