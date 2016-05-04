29085f216aedf43325fb876b428aaa304ca26b5b
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-05-04 11:12:44 -0400

    script for point function experiments

---
+++ obf-experiments/point.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -e
+
+BIN="build/bin/run-obfuscator"
+DIR="obf-experiments"
+CIRCUIT_DIR="$DIR/circuits"
+LOG_DIR="$DIR/runs"
+
+MIN=8
+MAX=16
+INC=4
+
+echo "* Running point functions ($MIN -> $MAX)"
+
+secparams=16
+
+for secparam in $secparams; do
+    echo "** security parameter: $secparam"
+    for point in `seq $MIN $INC $MAX`; do
+        for circuit in "point-$point.circ" "point-$point.json"; do
+            echo "**** circuit: $circuit"
+            for mmap in CLT GGH; do
+                echo "****** multilinear map: $mmap"
+                dir="$LOG_DIR/point/$secparam/$point/$circuit/$mmap"
+                mkdir -p $dir
+                obf=$circuit.obf.$secparam
+                eval=`python -c "print('0' * $point)"`
+
+                # obfuscate
+                $BIN obf \
+                     --load $CIRCUIT_DIR/$circuit \
+                     --secparam $secparam \
+                     --mlm $mmap \
+                     --sahai-zhandry \
+                     --verbose 2> $dir/obf-time.log
+                # get size of obfuscation
+                du --bytes $CIRCUIT_DIR/$obf/* > $dir/obf-size.log
+                # evaluate
+                $BIN obf \
+                     --load-obf $CIRCUIT_DIR/$obf \
+                     --eval $eval \
+                     --mlm $mmap \
+                     --sahai-zhandry \
+                     --verbose 2> $dir/eval-time.log
+                # cleanup
+                rm -rf $CIRCUIT_DIR/$circuit.obf.$secparam
+            done
+        done
+    done
+done
