241dd561dff935aa33c6acd6c2ef23b1ea484613
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-05-04 11:36:49 -0400

    add zimmerman experiments, and change point.sh -> point-sz.sh

---
+++ obf-experiments/point-zimmerman.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+
+BIN="build/bin/run-obfuscator"
+DIR="obf-experiments"
+CIRCUIT_DIR="$DIR/circuits"
+LOG_DIR="$DIR/runs"
+
+#
+# Change below as needed
+#
+
+###########################
+SECPARAMS="8 16"
+MIN=8
+MAX=16
+INC=4
+###########################
+
+echo "* Running point functions ($MIN -> $MAX)"
+
+for secparam in $SECPARAMS; do
+    echo "** security parameter: $secparam"
+    for point in `seq $MIN $INC $MAX`; do
+        for circuit in "point-$point.acirc"; do
+            echo "**** circuit: $circuit"
+            for mmap in CLT; do
+                echo "****** multilinear map: $mmap"
+                dir="$LOG_DIR/point/$secparam/$point/$circuit/$mmap"
+                mkdir -p $dir
+                obf=$circuit.obf.$secparam
+                eval=`python -c "print('0' * $point)"`
+
+                # obfuscate
+                $BIN obf \
+                     --load $CIRCUIT_DIR/$circuit \
+                     --secparam $secparam \
+                     --mlm $mmap \
+                     --zimmerman \
+                     --verbose 2> $dir/obf-time.log
+                # get size of obfuscation
+                du --bytes $CIRCUIT_DIR/$obf/* > $dir/obf-size.log
+                # evaluate
+                $BIN obf \
+                     --load-obf $CIRCUIT_DIR/$obf \
+                     --eval $eval \
+                     --mlm $mmap \
+                     --zimmerman \
+                     --verbose 2> $dir/eval-time.log
+                # cleanup
+                rm -rf $CIRCUIT_DIR/$circuit.obf.$secparam
+            done
+        done
+    done
+done
