d00c5ef22a53220b593ab2bfbd5b7adb71614545
Brent Carmer <bcarmer@gmail.com>
2016-06-16 09:54:59 -0700

    checkout master for all libraries in setup phase

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 builddir=$(readlink -f build)
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$builddir/lib"
+export PYTHONPATH="$builddir/lib/python2.7/site-packages:$builddir/lib64/python2.7/site-packages"
