2142c08116764b7bba2a7cc38f815dad74a4bfe9
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-10-16 13:51:38 -0700

    add parallelization flags to 3dnf/ore

--- runall.sh
+++ runall.sh
@@ -23,36 +23,36 @@ echo "**************************************************************"
 echo "* Running ORE experiments                                    *"
 echo "**************************************************************"
 pushd mife-experiments
-echo "CLT $small 4 18"
-./run-ore.sh CLT $small 4 18
+echo "CLT $small 4 18 1"
+./run-ore.sh CLT $small 4 18 1
 du -h
 ./clean.sh
-echo "CLT $small 4 21"
-./run-ore.sh CLT $small 4 21
+echo "CLT $small 4 21 1"
+./run-ore.sh CLT $small 4 21 1
 du -h
 ./clean.sh
-echo "GGH $small 4 18"
-./run-ore.sh GGH $small 4 18
+echo "GGH $small 4 18 1"
+./run-ore.sh GGH $small 4 18 1
 du -h
 ./clean.sh
-echo "GGH $small 4 21"
-./run-ore.sh GGH $small 4 21
+echo "GGH $small 4 21 1"
+./run-ore.sh GGH $small 4 21 1
 du -h
 ./clean.sh
-echo "CLT $big 4 18"
-./run-ore.sh CLT $big   4 18
+echo "CLT $big 4 18 1"
+./run-ore.sh CLT $big   4 18 1
 du -h
 ./clean.sh
-echo "CLT $big 4 21"
-./run-ore.sh CLT $big   4 21
+echo "CLT $big 4 21 1"
+./run-ore.sh CLT $big   4 21 1
 du -h
 ./clean.sh
-echo "GGH $big 4 18"
-./run-ore.sh GGH $big   4 18
+echo "GGH $big 4 18 0"
+./run-ore.sh GGH $big   4 18 0
 du -h
 ./clean.sh
-echo "GGH $big 5 19"
-./run-ore.sh GGH $big   5 19
+echo "GGH $big 5 19 0"
+./run-ore.sh GGH $big   5 19 0
 du -h
 ./clean.sh
 popd
@@ -62,20 +62,20 @@ echo "**************************************************************"
 echo "* Running 3DNF experiments                                   *"
 echo "**************************************************************"
 pushd mife-experiments
-echo "CLT $small 4 16"
-./run-3dnf.sh CLT $small 4 16
+echo "CLT $small 4 16 1"
+./run-3dnf.sh CLT $small 4 16 1
 du -h
 ./clean.sh
-echo "CLT $big 4 16"
-./run-3dnf.sh CLT $big   4 16
+echo "CLT $big 4 16 1"
+./run-3dnf.sh CLT $big   4 16 1
 du -h
 ./clean.sh
-echo "GGH $small 4 16"
-./run-3dnf.sh GGH $small 4 16
+echo "GGH $small 4 16 0"
+./run-3dnf.sh GGH $small 4 16 0
 du -h
 ./clean.sh
-echo "GGH $big 4 16"
-./run-3dnf.sh GGH $big   4 16
+echo "GGH $big 4 16 0"
+./run-3dnf.sh GGH $big   4 16 0
 du -h
 ./clean.sh
 popd
