820e7d736e40429b41ddedeb8122df245b409b63
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-10-16 13:46:25 -0700

    more verbose

--- runall.sh
+++ runall.sh
@@ -7,10 +7,15 @@ big=80
 echo "**************************************************************"
 echo "* Running obfuscation experiments                            *"
 echo "**************************************************************"
+echo "CLT $small 6-16 0"
 ./obf-experiments/point.sh CLT $small 6-16 0
+echo "CLT $small 7-29 0"
 ./obf-experiments/point.sh CLT $small 7-29 0
+echo "CLT $big 8-27 0"
 ./obf-experiments/point.sh CLT $big   8-27 0
+echo "GGH $small 9-13 0"
 ./obf-experiments/point.sh GGH $small 9-13 0
+echo "GGH $small 6-31 0"
 ./obf-experiments/point.sh GGH $small 6-31 0
 
 # ore experiments
@@ -18,27 +23,35 @@ echo "**************************************************************"
 echo "* Running ORE experiments                                    *"
 echo "**************************************************************"
 pushd mife-experiments
+echo "CLT $small 4 18"
 ./run-ore.sh CLT $small 4 18
 du -h
 ./clean.sh
+echo "CLT $small 4 21"
 ./run-ore.sh CLT $small 4 21
 du -h
 ./clean.sh
+echo "GGH $small 4 18"
 ./run-ore.sh GGH $small 4 18
 du -h
 ./clean.sh
+echo "GGH $small 4 21"
 ./run-ore.sh GGH $small 4 21
 du -h
 ./clean.sh
+echo "CLT $big 4 18"
 ./run-ore.sh CLT $big   4 18
 du -h
 ./clean.sh
+echo "CLT $big 4 21"
 ./run-ore.sh CLT $big   4 21
 du -h
 ./clean.sh
+echo "GGH $big 4 18"
 ./run-ore.sh GGH $big   4 18
 du -h
 ./clean.sh
+echo "GGH $big 5 19"
 ./run-ore.sh GGH $big   5 19
 du -h
 ./clean.sh
@@ -49,15 +62,19 @@ echo "**************************************************************"
 echo "* Running 3DNF experiments                                   *"
 echo "**************************************************************"
 pushd mife-experiments
+echo "CLT $small 4 16"
 ./run-3dnf.sh CLT $small 4 16
 du -h
 ./clean.sh
+echo "CLT $big 4 16"
 ./run-3dnf.sh CLT $big   4 16
 du -h
 ./clean.sh
+echo "GGH $small 4 16"
 ./run-3dnf.sh GGH $small 4 16
 du -h
 ./clean.sh
+echo "GGH $big 4 16"
 ./run-3dnf.sh GGH $big   4 16
 du -h
 ./clean.sh
