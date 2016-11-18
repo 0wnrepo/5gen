c5e25479f41a670a6b13f333493ffa508c2975cc
Alex J. Malozemoff <amaloz@galois.com>
2016-11-17 16:08:40 -0800

    better build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-#abort if any command fails
 set -e
 
 if [ "$1" == "debug" ]; then
@@ -10,108 +9,53 @@ else
     debugflag=''
 fi
 
-echo "libaesrand"
-    path=libaesrand
-    url=https://github.com/5GenCrypto/libaesrand.git
-    if [ ! -d $path ]; then
-        git clone $url;
-    else
-        cd $path; git pull origin master; cd ..;
-    fi
-
-echo "clt13"
-    path=clt13
-    url=https://github.com/5GenCrypto/clt13.git
-    if [ ! -d $path ]; then
-        git clone $url;
-    else
-        cd $path; git pull origin master; cd ..;
-    fi
-
-echo "gghlite-flint"
-    path=gghlite-flint
-    url=https://github.com/5GenCrypto/gghlite-flint.git
-    if [ ! -d $path ]; then
-        git clone $url;
-    else
-        cd $path; git pull origin master; cd ..;
-    fi
-
-echo "obfuscation"
-    path=obfuscation
-    url=https://github.com/5GenCrypto/obfuscation.git
-    if [ ! -d $path ]; then
-        git clone $url;
-    else
-        cd $path; git pull origin master; cd ..;
-    fi
-
-echo "libmmap"
-    path=libmmap
-    url=https://github.com/5GenCrypto/libmmap
-    if [ ! -d $path ]; then
-        git clone $url;
-    else
-        cd $path; git pull origin master; cd ..;
-    fi
-
-echo "mife"
-    path=mife
-    url=https://github.com/5GenCrypto/mife
-    if [ ! -d $path ]; then
-        git clone $url;
-    else
-        cd $path; git pull origin master; cd ..;
-    fi
-
 mkdir -p build
 builddir=$(readlink -f build)
+echo builddir = $builddir
 
 export CPPFLAGS=-I$builddir/include
 export CFLAGS=-I$builddir/include
 export LDFLAGS=-L$builddir/lib
 
-echo builddir = $builddir
+pull () {
+    path=$1
+    url=$2
+    branch=$3
+    if [ ! -d $path ]; then
+        git clone $url $path
+    fi
+    pushd $path
+        git pull origin $branch
+        if [ "$4" != "" ]; then
+            git checkout $4
+        fi
+    popd
+}
 
-echo building libaesrand
-cd libaesrand
-    autoreconf -i
-    ./configure --prefix=$builddir $debugflag
-    make -j
-    make check
-    make install
-cd ..
+build () {
+    path=$1
+    pushd $path
+        mkdir -p build/autoconf
+        autoreconf -i
+        ./configure --prefix=$builddir $debugflag
+        make -j
+        # make check
+        make install
+    popd
+}
 
-echo building clt13
-cd clt13
-    mkdir -p build/autoconf
-    autoreconf -i
-    ./configure --prefix=$builddir $debugflag
-    make -j
-    make check
-    make install
-cd ..
+pull libaesrand  https://github.com/5GenCrypto/libaesrand    master
+pull clt13       https://github.com/5GenCrypto/clt13         master
+pull gghlite     https://github.com/5GenCrypto/gghlite-flint master
+pull libmmap     https://github.com/5GenCrypto/libmmap       master
+pull mife        https://github.com/5GenCrypto/mife          master
+pull obfuscation https://github.com/5GenCrypto/obfuscation   master
 
-echo building gghlite-flint
-cd gghlite-flint
-    autoreconf -i
-    ./configure --prefix=$builddir $debugflag
-    make -j
-    make check
-    make install
-cd ..
-
-echo building libmmap
-cd libmmap
-    autoreconf -i
-    ./configure --prefix=$builddir $debugflag
-    make -j
-    make check
-    make install
-cd ..
-
-echo building mife
-cd mife
+build libaesrand
+build clt13
+build gghlite
+build libmmap
+pushd mife
     git submodule init
     git submodule update
     mkdir -p build/autoconf
@@ -121,13 +65,12 @@ cd mife
     mv tmp-jsmn-makefile mife/jsmn/Makefile
     make -j
     make install
-cd ..
-
-echo building obfuscation
-cd obfuscation
+popd
+pushd obfuscation
+    mkdir -p build/autoconf
     autoreconf -i
     ./configure --prefix=$builddir $debugflag
-    make
+    make -j
     make install
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$builddir/lib"
     export PYTHONPATH="$builddir/lib/python2.7/site-packages:$builddir/lib64/python2.7/site-packages"
@@ -135,7 +78,7 @@ cd obfuscation
     mkdir -p $builddir/lib/python2.7/site-packages
     mkdir -p $builddir/lib64/python2.7/site-packages
     python2 setup.py install --prefix=$builddir
-cd ..
+popd
 
 cat << EOF > $builddir/bin/run-obfuscator
 #!/usr/bin/env bash
