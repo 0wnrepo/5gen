df869e113f67ca56890b08061a827a353a2ff4fe
Alex J. Malozemoff <amaloz@galois.com>
2017-03-23 09:22:36 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 builddir=$(readlink -f build)
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$builddir/lib"
-export PYTHONPATH="$builddir/lib/python2.7/site-packages:$builddir/lib64/python2.7/site-packages"
+export PYTHONPATH="$PYTHONPATH:$builddir/lib/python2.7/site-packages:$builddir/lib64/python2.7/site-packages"
